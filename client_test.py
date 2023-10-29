27df4835a291032ab1154333b461432f59fe6261
dubba1212 <reddysri1999@gmail.com>
2023-10-29 02:50:39 -0400

    Second commit

--- client_test.py
+++ client_test.py
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],
+                                             (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +18,24 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],
+                                             (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def calculatePriceBidGreaterThanAskquotes(quotes):
+    result = []
+    for quote in quotes:
+      bid_price = quote.get('top_bid_price', 0)  # Get the bid price (default to 0 if not present)
+      ask_price = quote.get('top_ask_price', 0)  # Get the ask price (default to 0 if not present)
 
+      if bid_price > ask_price:
+        # Calculate the price when bid is greater than ask
+        price = (bid_price + ask_price) / 2
+        result.append(price)
+
+    return result
 
 
 if __name__ == '__main__':
